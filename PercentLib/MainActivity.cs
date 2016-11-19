3ffb36d017e2628874933567a93c215f277908b7
suchithm <suchimadavu@gmail.com>
2016-11-19 21:16:34 +0530

    Initial check-in of module PercentLib

---
+++ PercentLib/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace PercentLib
+{
+	[Activity(Label = "Android Percent Library", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{ 
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			RequestWindowFeature(Android.Views.WindowFeatures.NoTitle);
+			base.OnCreate(savedInstanceState); 
+			SetContentView(Resource.Layout.Main); 
+		}
+	}
+}
